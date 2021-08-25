b80615490ca8780ca299962c1923abfdd59ad344
bcharlotin1 <biancacharlotin1@gmail.com>
2021-08-25 13:17:33 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+const mainElement = document.getElementById('main');
+
+mainElement.remove();
+
+let newHeader = document.createElement('h1');
+newHeader.setAttribute('id', 'victory')
+document.body.appendChild(newHeader);
+newHeader.innerHTML = `is the champion`;
